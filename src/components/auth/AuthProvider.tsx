8f4270dcb3f89378d29c6935f1c751a2396f9b75
zamminhduy123 <webhosting.tbn@gmail.com>
2026-04-21 09:55:23 +0700

    Performance adjust for auth route

--- src/components/auth/AuthProvider.tsx
+++ src/components/auth/AuthProvider.tsx
@@ -4,5 +4,12 @@ import { SessionProvider } from 'next-auth/react';
 import React from 'react';
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider 
+      refetchOnWindowFocus={false} // Prevents a request every time you switch tabs/windows
+      refetchInterval={5 * 60}     // Only refresh session every 5 minutes (optional)
+    >
+      {children}
+    </SessionProvider>
+  );
 }
